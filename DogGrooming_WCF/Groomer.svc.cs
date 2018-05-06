a10544f93edc9567fdd86b99dec23d88aed26713
DESKTOP-R9STC8I\hhu88 <jessicahu@windowslive.com>
2018-05-06 21:48:07 +1000

    Created WCF Service which only use GET

---
+++ DogGrooming_WCF/Groomer.svc.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DogGrooming_WCF
+{
+    public class Groomer : IGroomer
+    {
+        public string AuthenticateGroomer(string groomerEmail, string groomerPassword)
+        {
+            return Authenticate(groomerEmail, groomerPassword) ? "Success" : "Fail";
+        }
+
+        public string CreateGroomer(string firstname, string surname, string email, string password)
+        {
+            int idGroomer = Create(firstname, surname, email, password);
+            if (idGroomer > 0) return "Success: " + idGroomer;
+            else return "Fail: User already exist";
+        }
+
+        public string DeleteGroomer(string idGroomer)
+        {
+            if (int.TryParse(idGroomer, out int id))
+            {
+                if (Delete(id)) return "Success";
+                else return "Fail: Cannot delete groomer";
+            }
+            else
+            {
+                return "Fail: Invalid idGroomer";
+            }
+        }
+
+        public Dictionary<string, string> GetGroomerById(string idGroomer)
+        {
+            if (int.TryParse(idGroomer, out int id)) return GetById(id);
+            else return null;
+        }
+
+        public List<Dictionary<string, string>> GetGroomerList()
+        {
+            return GetAll();
+        }
+
+        public string UpdateGroomer(string idGroomer, string firstname, string surname, string email, string password)
+        {
+            if (int.TryParse(idGroomer, out int id))
+            {
+                if (Update(id, firstname, surname, email, password)) return "Success";
+                else return "Fail: Cannot update groomer";
+            }
+            else
+            {
+                return "Fail: Invalid idGroomer";
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+        //====================================================//
+        //======================= LOGIC ======================//
+        //====================================================//
+        
+
+        private bool Authenticate(string email, string password)
+        {
+            return true;
+        }
+
+        private int Create(string firstname, string surname, string email, string password)
+        {
+            // Insert into database
+            // Retreve idGroomer and send it back
+            return 1;
+        }
+
+        private bool Delete(int idGroomer)
+        {
+            // Delete groomer and appointments from the database
+            return true;
+        }
+
+        private Dictionary<string, string> GetById(int idGroomer)
+        {
+            // Retreve idGroomer
+            // Put information into a dictionary
+            var groomerDetails = new Dictionary<string, string>();
+            groomerDetails.Add("idGroomer", "1");
+            groomerDetails.Add("FirstName", "Tom");
+            groomerDetails.Add("Email", "tomgroomer@gmail.com");
+
+            // Send dictionary back
+            return groomerDetails;
+        }
+
+        private List<Dictionary<string, string>> GetAll()
+        {
+            // Retrieve all groomers
+            var allGroomers = new List<Dictionary<string, string>>();
+            // Put information in a list of dictionary
+
+            var groomerDetails = new Dictionary<string, string>();
+            groomerDetails.Add("idGroomer", "1");
+            groomerDetails.Add("FirstName", "Tom");
+            groomerDetails.Add("Email", "tomgroomer@gmail.com");
+
+            allGroomers.Add(groomerDetails);
+
+            // Send list of dictionary back
+            return allGroomers;
+        }
+
+        private bool Update(int idGroomer, string firstname, string surname, string email, string password)
+        {
+            // Update groomer details
+            // Sent status
+            return true;
+        }
+
+    }
+}
---
+++ DogGrooming_WCF/Service.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace DogGrooming_WCF
+{
+    [ServiceContract]
+    public interface IGroomer
+    {
+        [OperationContract]
+        [WebGet(UriTemplate="ViewAll")]
+        List<Dictionary<string, string>> GetGroomerList();
+
+        [OperationContract]
+        [WebGet(UriTemplate = "View/{idGroomer}")]
+        Dictionary<string, string> GetGroomerById(string idGroomer);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Add/{firstName}/{surname}/{email}/{password}")]
+        string CreateGroomer(string firstname, string surname, string email, string password);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Update/{idGroomer}/{firstName}/{surname}/{email}/{password}")]
+        string UpdateGroomer(string idGroomer, string firstname, string surname, string email, string password);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Delete/{idGroomer}")]
+        string DeleteGroomer(string idGroomer);
+
+        [OperationContract]
+        [WebGet(UriTemplate = "Authenticate/{groomerEmail}/{groomerPassword}")]
+        string AuthenticateGroomer(string groomerEmail, string groomerPassword);
+    }
+
+
+    [ServiceContract]
+    public interface IAppointment
+    {
+        [OperationContract]
+        List<Dictionary<string, string>> GetAppointmentList();
+
+        [OperationContract]
+        Dictionary<string, string> GetAppointmentById(string idGroomer, string idDog, string startTime);
+
+        [OperationContract]
+        int CreateAppointment(Dictionary<string, string> appointmentDetails);
+
+        [OperationContract]
+        bool DeleteAppointment(string idGroomer, string idDog, string startTime);
+    }
+
+
+    [ServiceContract]
+    public interface IDog
+    {
+        [OperationContract]
+        List<Dictionary<string, string>> GetDogList();
+
+        [OperationContract]
+        Dictionary<string, string> GetDogById(string idDog);
+
+        [OperationContract]
+        int CreateDog(Dictionary<string, string> dogDetails);
+
+        [OperationContract]
+        bool UpdateDog(Dictionary<string, string> dogDetailsAndId);
+
+        [OperationContract]
+        bool DeleteDog(string idDog);
+    }
+
+    [ServiceContract]
+    public interface IClient
+    {
+        [OperationContract]
+        List<Dictionary<string, string>> GetClientList();
+
+        [OperationContract]
+        Dictionary<string, string> GetClientById(string idClient);
+
+        [OperationContract]
+        int CreateClient(Dictionary<string, string> clientDetails);
+
+        [OperationContract]
+        bool UpdateClient(Dictionary<string, string> clientDetailsAndId);
+
+        [OperationContract]
+        bool DeleteClient(string idClient);
+
+        [OperationContract]
+        bool AuthenticateClient(Dictionary<string, string> clientEmailAndPassword);
+    }
+
+
+    [ServiceContract]
+    public interface IBreed
+    {
+        [OperationContract]
+        List<Dictionary<string, string>> GeBreedList();
+
+        [OperationContract]
+        Dictionary<string, string> GetBreedById(string idBreed);
+
+        [OperationContract]
+        int CreateBreed(Dictionary<string, string> breedDetails);
+
+        [OperationContract]
+        bool DeleteBreed(string idDog);
+    }
+
+
+    [ServiceContract]
+    public interface IGroomingType
+    {
+        [OperationContract]
+        List<Dictionary<string, string>> GeGroomingTypeList();
+
+        [OperationContract]
+        Dictionary<string, string> GetGroomingById(string idGroomingType);
+
+        [OperationContract]
+        int CreateGroomingType(Dictionary<string, string> groomingTypeDetails);
+
+        [OperationContract]
+        bool DeleteGroomingType(string idGroomingType);
+    }
+
+}
